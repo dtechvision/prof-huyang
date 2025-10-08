577a57d9ecbb321a0993e187df252d045b6bd525
Samuel Huber <samuel@dtech.vision>
2025-10-08 23:42:06 +0200

    update schema

--- drizzle/0000_regular_virginia_dare.sql
+++ drizzle/0000_regular_virginia_dare.sql
@@ -1,49 +1,55 @@
 -- Current sql file was generated after introspecting the database
 -- If you want to run this migration please uncomment this code before executing migrations
 /*
-CREATE TABLE "casts" (
-	"fid" bigint NOT NULL,
-	"hash" text NOT NULL,
-	"timestamp" timestamp with time zone,
-	"embeds" jsonb[],
-	"parent_cast_url" text,
-	"parent_cast_fid" bigint,
-	"parent_cast_hash" text,
-	"text" text,
-	"mentions" integer[],
-	"mentions_positions" integer[],
-	"deleted_at" timestamp with time zone
-);
---> statement-breakpoint
 CREATE TABLE "links" (
-	"fid" bigint NOT NULL,
+	"fid" text NOT NULL,
 	"timestamp" timestamp with time zone,
-	"target_fid" bigint NOT NULL,
+	"signer" text,
+	"target_fid" text NOT NULL,
 	"type" text NOT NULL,
 	"deleted_at" timestamp with time zone
 );
 --> statement-breakpoint
 CREATE TABLE "reactions" (
-	"fid" bigint NOT NULL,
+	"fid" text NOT NULL,
 	"timestamp" timestamp with time zone,
-	"target_cast_fid" bigint NOT NULL,
+	"signer" text,
+	"target_cast_fid" text NOT NULL,
 	"target_cast_hash" text NOT NULL,
 	"type" text NOT NULL,
 	"deleted_at" timestamp with time zone
 );
 --> statement-breakpoint
 CREATE TABLE "profiles" (
-	"fid" bigint NOT NULL,
+	"fid" text NOT NULL,
 	"data" jsonb,
 	"custody_address" text,
+	"signer" text,
 	"last_updated_at" timestamp with time zone
 );
 --> statement-breakpoint
-CREATE TABLE "verifications" (
-	"fid" bigint NOT NULL,
-	"address" text NOT NULL,
+CREATE TABLE "casts" (
+	"fid" text NOT NULL,
+	"hash" text NOT NULL,
 	"timestamp" timestamp with time zone,
+	"signer" text,
+	"embeds" jsonb[],
+	"parent_cast_url" text,
+	"parent_cast_fid" text,
+	"parent_cast_hash" text,
+	"text" text,
+	"mentions" text[],
+	"mentions_positions" integer[],
 	"deleted_at" timestamp with time zone
 );
-
+--> statement-breakpoint
+CREATE TABLE "verifications" (
+	"fid" text NOT NULL,
+	"address" text NOT NULL,
+	"timestamp" timestamp with time zone,
+	"signer" text,
+	"deleted_at" timestamp with time zone
+);
+--> statement-breakpoint
+CREATE INDEX "casts_fid_timestamp_idx" ON "casts" USING btree ("fid" text_ops,"timestamp" text_ops);
 */
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,57 +1,56 @@
-import { pgTable, bigint, text, timestamp, jsonb, integer } from "drizzle-orm/pg-core"
+import { pgTable, text, timestamp, jsonb, index, integer } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 
 
 
-export const casts = pgTable("casts", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	fid: bigint({ mode: "number" }).notNull(),
-	hash: text().notNull(),
-	timestamp: timestamp({ withTimezone: true, mode: 'string' }),
-	embeds: jsonb(),
-	parentCastUrl: text("parent_cast_url"),
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	parentCastFid: bigint("parent_cast_fid", { mode: "number" }),
-	parentCastHash: text("parent_cast_hash"),
-	text: text(),
-	mentions: integer(),
-	mentionsPositions: integer("mentions_positions"),
-	deletedAt: timestamp("deleted_at", { withTimezone: true, mode: 'string' }),
-});
-
 export const links = pgTable("links", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	fid: bigint({ mode: "number" }).notNull(),
+	fid: text().notNull(),
 	timestamp: timestamp({ withTimezone: true, mode: 'string' }),
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	targetFid: bigint("target_fid", { mode: "number" }).notNull(),
+	signer: text(),
+	targetFid: text("target_fid").notNull(),
 	type: text().notNull(),
 	deletedAt: timestamp("deleted_at", { withTimezone: true, mode: 'string' }),
 });
 
 export const reactions = pgTable("reactions", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	fid: bigint({ mode: "number" }).notNull(),
+	fid: text().notNull(),
 	timestamp: timestamp({ withTimezone: true, mode: 'string' }),
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	targetCastFid: bigint("target_cast_fid", { mode: "number" }).notNull(),
+	signer: text(),
+	targetCastFid: text("target_cast_fid").notNull(),
 	targetCastHash: text("target_cast_hash").notNull(),
 	type: text().notNull(),
 	deletedAt: timestamp("deleted_at", { withTimezone: true, mode: 'string' }),
 });
 
 export const profiles = pgTable("profiles", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	fid: bigint({ mode: "number" }).notNull(),
+	fid: text().notNull(),
 	data: jsonb(),
 	custodyAddress: text("custody_address"),
+	signer: text(),
 	lastUpdatedAt: timestamp("last_updated_at", { withTimezone: true, mode: 'string' }),
 });
 
+export const casts = pgTable("casts", {
+	fid: text().notNull(),
+	hash: text().notNull(),
+	timestamp: timestamp({ withTimezone: true, mode: 'string' }),
+	signer: text(),
+	embeds: jsonb(),
+	parentCastUrl: text("parent_cast_url"),
+	parentCastFid: text("parent_cast_fid"),
+	parentCastHash: text("parent_cast_hash"),
+	text: text(),
+	mentions: text(),
+	mentionsPositions: integer("mentions_positions"),
+	deletedAt: timestamp("deleted_at", { withTimezone: true, mode: 'string' }),
+}, (table) => [
+	index("casts_fid_timestamp_idx").using("btree", table.fid.asc().nullsLast().op("text_ops"), table.timestamp.desc().nullsFirst().op("text_ops")),
+]);
+
 export const verifications = pgTable("verifications", {
-	// You can use { mode: "bigint" } if numbers are exceeding js number limitations
-	fid: bigint({ mode: "number" }).notNull(),
+	fid: text().notNull(),
 	address: text().notNull(),
 	timestamp: timestamp({ withTimezone: true, mode: 'string' }),
+	signer: text(),
 	deletedAt: timestamp("deleted_at", { withTimezone: true, mode: 'string' }),
 });
